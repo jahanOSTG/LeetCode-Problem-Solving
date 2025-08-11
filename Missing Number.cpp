c17f88e01577c938ee689d406b2ecd5b3f43f1d1
Faria Jahan Janie <janiefaria315@gmail.com>
2025-08-11 21:21:01 +0600

    Create Missing Number.cpp

---
+++ Missing Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum_of_num=n*(n+1)/2;
+        int arr_sum=0;
+        for(int i=0;i<n;i++){
+            arr_sum=arr_sum+nums[i];
+        }
+        return sum_of_num-arr_sum;
+
+
+        
+    }
+};
